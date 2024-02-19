f57aa2363616da4a813350c3a4438f89c294f7a1
rishabhkr-r111 <rishabhkrr111@gmail.com>
2024-02-19 23:39:33 +0530

    creating new chats based on ips

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import os
 import json
 import google.generativeai as genai
 from dotenv import load_dotenv
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
 from typing import List, Dict
 import uvicorn
 
 load_dotenv()
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat(history=[])
 
 app = FastAPI()
+
+chats: Dict[str, genai.GenerativeModel] = {}
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    ip_address = websocket.client.host
+    chats[ip_address] = model.start_chat(history=[])
     while True:
         message = await websocket.receive_text()
-        print(message)
         if message == "!<FIN>!":
-                websocket.close()
-                break
+            websocket.close()
+            break
         else:
-            response = chat.send_message(message, stream=True)
+            response = chats[ip_address].send_message(message, stream=True)
             for msg in response:
                 await websocket.send_text(msg.text)
             await websocket.send_text("<FIN>")
